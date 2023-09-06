2e1caa4fedd9d45fe10fba3db348495d96c1de85
tottyunair <tottyrumbiak@gmail.com>
2023-09-06 23:30:37 +0700

    buat portofolio

---
+++ resources/views/komponen/porto.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Magang's Team Portofolio</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="navbar-background">
+                <ul>
+                    <li>
+                        <div class="logo">
+                            <img src="" alt="LogoTeamMagang">
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="services">Services</div>
+                    </li>
+
+                    <li>
+                        <div class="portofolios">Portofolios</div>
+                    </li>
+
+                    <li>
+                        <div class="experience">Experience</div>
+                    </li>
+
+                    <li>
+                        <div class="blog">Blog</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="main-content">
+            <div class="main-content-background">
+                
+                <div class="homebarsec1">
+                    <div class="homebar1">
+                        <h1>Magang Team</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias enim, maxime non autem dignissimos fuga impedit tenetur distinctio quae temporibus similique. Dignissimos eligendi culpa, repellendus magni possimus quae officiis facere! Itaque, dignissimos deserunt fuga repudiandae mollitia beatae possimus doloremque deleniti recusandae esse labore provident, sapiente non reiciendis aperiam corporis eum!</p>
+                    </div>
+                    <div class="homebar1-logo">
+                        <img src="" alt="Logo Team">
+                    </div>
+                </div>
+
+                <div class="homebarsec2">
+                    <title>Specialized in</title>
+                    <div class="homebar2">
+                        <li>
+                            <div class="card-homebar2">
+                                <div class="logo-card-homebar2">
+                                    <img src="" alt="">
+                                </div>
+                                <h2>UI/UX Design</h2>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fuga optio commodi asperiores ipsa facere accusamus? Quas nesciunt maxime dolorem.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar2">
+                                <div class="logo-card-homebar2">
+                                    <img src="" alt="">
+                                </div>
+                                <h2>Web Development</h2>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fuga optio commodi asperiores ipsa facere accusamus? Quas nesciunt maxime dolorem.</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar2">
+                                <div class="logo-card-homebar2">
+                                    <img src="" alt="">
+                                </div>
+                                <h2>Application Development</h2>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fuga optio commodi asperiores ipsa facere accusamus? Quas nesciunt maxime dolorem.</p>
+                            </div>
+                        </li>
+                    </div>
+                </div>
+
+                <div class="homebarsec3">
+                    <title>Team'S Portofolio</title>
+                    <div class="homebar3">
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+                    </div>
+                </div>
+
+                <div class="homebarsec4">
+                    <title>Photo'S</title>
+                    <div class="homebar4">
+                        <li>
+                            <div class="card-homebar4">
+                                <div class="logo-card-homebar4">
+                                    <img src="" alt="FOTO">
+                                </div>
+                                <h1>FOTO 1</h1>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div class="card-homebar3">
+                                <div class="logo-card-homebar3">
+                                    <img src="" alt="FOTO-Porto">
+                                </div>
+                                <h1>F.Totty Valentino Yohanes Rumbiak - Sistem Informasi</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae sunt sequi, quas reprehenderit aspernatur eveniet id dolore voluptate, nisi quasi, recusandae repellendus voluptatibus ullam amet odio autem. Delectus eveniet blanditiis voluptate nostrum recusandae laudantium adipisci obcaecati est illo tempora labore maiores aliquam maxime, dolorem ex? Sequi aliquam voluptatibus accusamus?</p>
+                            </div>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
