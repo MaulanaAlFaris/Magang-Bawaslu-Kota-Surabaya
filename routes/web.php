e262e446f1c44c26fd0a868e0dd1ea52f13149c5
tottyunair <tottyrumbiak@gmail.com>
2023-09-07 00:08:00 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -966,7 +966,7 @@ Route::resource('KelurahanKetabang',KelurahanKetabangController::class);
 //Routing PDF
 Route::get('KelurahanKetabang/pdf/{id}', [KelurahanKetabangController::class,'pdf'])->name('KelurahanKetabang.pdf');
 //Routing Search Bar
-Route::get('searchKelKapasari', [KelurahanKetabangController::class, 'searchKelKapasari'])->name('KelurahanKetabang.search');
+Route::get('searchKelKetabang', [KelurahanKetabangController::class, 'searchKelKetabang'])->name('KelurahanKetabang.search');
 
 Route::resource('KelurahanPeneleh',KelurahanPenelehController::class);
 //Routing PDF
