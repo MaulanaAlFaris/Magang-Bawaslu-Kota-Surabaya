6ff6500eea2d4bda761750b9895f9642d8363471
anantaoktav <132391887+anantaoktav@users.noreply.github.com>
2023-09-06 22:16:36 +0700

    update

--- resources/views/content/kelurahan/Kecamatan_15_Tenggilis_Mejoyo/Kelurahan_Tenggilis_Mejoyo/index.blade.php
+++ resources/views/content/kelurahan/Kecamatan_15_Tenggilis_Mejoyo/Kelurahan_Tenggilis_Mejoyo/index.blade.php
@@ -43,6 +43,8 @@
                         <td>{{ $item->waktu_dan_tempat }}</td>
                         <td>
                             <a href='{{ url('KelurahanTenggilisMejoyo/'.$item->tahapan.'/edit') }}' class="btn btn-warning btn-sm">Edit</a>
+                            <a href='{{ url("KelurahanTenggilisMejoyo/".$item->tahapan) }}' class="btn btn-success btn-sm">PDF</a>
+
                             <form onsubmit="return confirm('Yakin akan menghapus data?')" class='d-inline' action="{{ url('KelurahanTenggilisMejoyo'.$item->tahapan) }}" method="post">
                                 @csrf
                                 @method('DELETE')
--- resources/views/content/kelurahan/Kecamatan_27_Lakarsantri/Kelurahan_Lidah_Kulon/index.blade.php
+++ resources/views/content/kelurahan/Kecamatan_27_Lakarsantri/Kelurahan_Lidah_Kulon/index.blade.php
@@ -43,6 +43,8 @@
                         <td>{{ $item->waktu_dan_tempat }}</td>
                         <td>
                             <a href='{{ url('KelurahanLidahKulon/'.$item->tahapan.'/edit') }}' class="btn btn-warning btn-sm">Edit</a>
+                            <a href='{{ url("KelurahanLidahKulon/".$item->tahapan) }}' class="btn btn-success btn-sm">PDF</a>
+
                             <form onsubmit="return confirm('Yakin akan menghapus data?')" class='d-inline' action="{{ url('KelurahanLidahKulon'.$item->tahapan) }}" method="post">
                                 @csrf
                                 @method('DELETE')
