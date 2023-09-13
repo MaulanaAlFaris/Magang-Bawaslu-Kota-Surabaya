54b8cf5a3aecd329b38cce01a65e79f414c556ba
tottyunair <tottyrumbiak@gmail.com>
2023-09-14 03:36:29 +0700

    update about

--- resources/views/komponen/porto.blade.php
+++ resources/views/komponen/porto.blade.php
@@ -21,25 +21,25 @@
 
                         <li>
                             <a href="#experience-id">
-                                <div class="experience">EXPERIENCE</div>
+                                <div class="experience">Struktur Organisasi</div>
                             </a>
                         </li>
 
                         <li>
                             <a href="#portofolio-id">
-                                <div class="portofolio">PORTOFOLIO</div>
+                                <div class="portofolio">Team Bawaslu</div>
                             </a>
                         </li>
 
                         <li>
                             <a href="#photo-id">
-                                <div class="photo">PHOTO</div>
+                                <div class="photo">Dokumentasi</div>
                             </a>
                         </li>
 
                         <li>
                             <a href="#contact-id">
-                                <div class="contact">CONTACT</div>
+                                <div class="contact">Contact</div>
                             </a>
                         </li>
                     </div>
@@ -54,60 +54,28 @@
                 <div class="homebarsec1" id="homebar-id">
                     <div class="homebarsec1-bg">
                         <div class="homebar1">
-                            <h2>Magang Team</h2>
-                            <p>Magang Team merupakan hasil kerja sama antara Universitas Airlangga dan Universitas 17 Agustus Surabaya dalam mengembangkan situs web proyek Form A yang dimiliki oleh Badan Pengawasan Pemilihan Umum Kota Surabaya. Kolaborasi ini bertujuan untuk meningkatkan pengalaman mahasiswa dalam pengembangan teknologi, khususnya di bidang IT, dalam memberikan dukungan teknis dan inovasi dalam proyek tersebut.</p>
-                        </div>
-                        <div class="homebar1-logo">
-                            <img src="{{ asset('images/foto.png') }}" alt="Logo Team">
+                            <h2>Badan Pengawasan Pemilihan Umum Kota Surabaya</h2>
+                            <p>Bawaslu Kota Surabaya mengacu pada Badan Pengawas Pemilihan Umum (Bawaslu) di Kota Surabaya, Indonesia. Bawaslu adalah lembaga yang bertanggung jawab untuk mengawasi pelaksanaan pemilihan umum dan pemilihan kepala daerah di Indonesia. Tugas utama Bawaslu adalah memastikan bahwa pemilihan berlangsung secara adil, jujur, dan sesuai dengan peraturan yang berlaku.</p>
                         </div>
                     </div>
                 </div>
 
                 <div class="homebarsec2" id="experience-id">
                     <div class="homebarsec2-title-bg">
-                        <h2>Experience</h2>
+                        <h2>Struktur Organisasi</h2>
                     </div>
                     <div class="homebarsec2-bg">
                         <div class="homebar2">
-                            <li>
-                                <div class="card-homebar2">
-                                    <div class="logo-card-homebar2">
-                                        <img src="{{ asset('images/uiux.png') }}" alt="#">
-                                    </div>
-                                    <h2>UI/UX Design</h2>
-                                    <p>UI Design (User Interface Design) adalah cara produk digital terlihat. Ini mencakup warna, bentuk, tombol, dan semua yang Anda lihat saat menggunakan aplikasi atau situs web. Tujuannya adalah membuatnya terlihat bagus dan mudah dipahami.</p>
-                                    <p>UX Design (User Experience Design) adalah tentang bagaimana produk digital berfungsi dan bagaimana pengguna berinteraksi dengannya. Ini mencakup apakah produknya mudah digunakan, membuat pengguna senang, dan memecahkan masalah pengguna dengan baik. Kedua desain ini sangat penting karena antarmuka yang baik (UI) dan pengalaman pengguna yang baik (UX) membuat produk digital lebih baik untuk digunakan oleh orang lain.</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <div class="card-homebar2">
-                                    <div class="logo-card-homebar2">
-                                        <img src="{{ asset('images/web.png') }}" alt="#">
-                                    </div>
-                                    <h2>Web Development</h2>
-                                    <p>Web development adalah proses menciptakan dan memelihara situs web atau aplikasi web. Ini mencakup desain tampilan depan (UI) yang dilihat oleh pengguna dan pengembangan back-end yang mengatur bagaimana situs web berfungsi.</p>
-                                    <p>Pengembang menggunakan bahasa pemrograman seperti HTML, CSS, JavaScript, dan bahasa back-end seperti PHP, Python, atau Ruby. Mereka juga mengelola basis data untuk menyimpan informasi. Pengujian dan optimisasi kinerja penting sebelum situs web diluncurkan. Keamanan juga menjadi fokus untuk melindungi data dan mencegah serangan. Setelah peluncuran, pemeliharaan berkala diperlukan untuk memastikan situs web tetap berjalan dengan baik dan aman. Tujuan utama adalah menciptakan situs web yang fungsional, menarik, dan responsif.</p>
-                                </div>
-                            </li>
-
-                            <li>
-                                <div class="card-homebar2">
-                                    <div class="logo-card-homebar2">
-                                        <img src="{{ asset('images/app.png') }}" alt="#">
-                                    </div>
-                                    <h2>Application Development</h2>
-                                    <p>Application development adalah proses merancang, membangun, dan mengembangkan aplikasi perangkat lunak untuk berbagai platform seperti mobile, desktop, dan web. Ini melibatkan pemilihan bahasa pemrograman, pengembangan antarmuka pengguna (UI), logika aplikasi, dan pengujian.</p>
-                                    <p>Pengembang aplikasi menciptakan solusi perangkat lunak yang memenuhi kebutuhan pengguna dengan fokus pada fungsionalitas, keamanan, dan kinerja. Mereka juga memelihara dan memperbarui aplikasi secara berkala untuk memastikan konsistensi dan keamanan. Tujuan utama adalah memberikan pengalaman yang efektif, efisien, dan memuaskan kepada pengguna melalui aplikasi yang dapat digunakan dengan mudah dan memecahkan masalah yang spesifik.</p>
-                                </div>
-                            </li>
+                            <div class="gambar-struktur">
+                                <img src="{{ asset('images/struktur.png') }}" alt="">
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="homebarsec3" id="portofolio-id">
                     <div class="homebarsec3-title-bg">
-                        <h2>Team Portofolio</h2>
+                        <h2>Team Bawaslu</h2>
                     </div>
                     <div class="homebarsec3-bg">
                         <div class="homebar3">
@@ -119,6 +87,7 @@
                                     <h2>F.Totty Valentino Yohanes Rumbiak</h2>
                                     <h2>Sistem Informasi</h2>
                                     <h2>Universitas Airlangga</h2>
+                                    <h2 style="color:black; background-color:orange">Full Stack Developer</h2>
                                     <p>Hai, saya F. Totty Valentino Yohanes Rumbiak, mahasiswa dari Universitas Airlangga. Saya memiliki minat kuat dalam teknologi dan pengembangan perangkat lunak. Selama studi saya, saya belajar pemrograman, desain aplikasi, dan pengembangan web. Saya aktif berkontribusi dalam proyek pengembangan perangkat lunak yang inovatif. Saya antusias untuk terus belajar dan meningkatkan keterampilan saya di dunia teknologi. Selain itu, saya juga senang berkolaborasi untuk menciptakan solusi bermanfaat dalam bidang teknologi informasi.</p>
                                 </div>
                             </li>
@@ -131,6 +100,7 @@
                                     <h2>Adhitya Tri Wicaksana</h2>
                                     <h2>Sistem Informasi</h2>
                                     <h2>Universitas Airlangga</h2>
+                                    <h2 style="color:black; background-color:orange">UI/UX Designer</h2>
                                     <p>Hai, saya Adhitya Tri Wicaksana Permana, tinggal di Surabaya. Saya selalu bersemangat untuk mengejar peluang-peluang pembelajaran dan pertumbuhan di dunia teknologi informasi (IT). Mengejar pengetahuan dan keterampilan baru dalam IT adalah prioritas saya, dan saya yakin bahwa dengan komitmen dan dedikasi saya, saya dapat mencapai banyak hal yang bermanfaat di bidang ini. Saya sangat antusias untuk terus belajar dan berkembang di dunia IT.</p>
                                 </div>
                             </li>
@@ -143,6 +113,7 @@
                                     <h2>Ananta Oktaviana</h2>
                                     <h2>Teknik Informatika</h2>
                                     <h2>Universitas 17 Agustus Surabaya</h2>
+                                    <h2 style="color:black; background-color:orange">Front End Dev</h2>
                                     <p>Hai, saya Ananta Oktavian. Saat ini, saya tengah mengejar pendidikan di Universitas 17 Agustus Surabaya, dengan jurusan Teknik Informatika. Selama perjalanan kuliahku, saya telah mengembangkan minat yang mendalam dalam dunia teknologi dan komputasi. Meskipun saya tinggal dan belajar di Surabaya, saya juga senang berpartisipasi dalam berbagai komunitas IT dan acara-acara teknologi di luar untuk memperluas jaringan dan pengetahuan saya.</p>
                                 </div>
                             </li>
@@ -155,6 +126,7 @@
                                     <h2>Eryanda Hermanto</h2>
                                     <h2>Teknik Informatika</h2>
                                     <h2>Universitas 17 Agustus Surabaya</h2>
+                                    <h2 style="color:black; background-color:orange">Front End Dev</h2>
                                     <p>Saya Eryanda Hermanto, asli Surabaya, saat ini tengah mengejar gelar sarjana di Universitas 17 Agustus 1945 Surabaya dengan jurusan Teknik Informatika. Keputusan saya untuk memilih jurusan ini didasari oleh minat mendalam dalam teknologi dan komputasi. Saya percaya bahwa melalui pendidikan ini, saya akan memperoleh pengetahuan dan keterampilan yang kuat dalam pengembangan perangkat lunak, analisis data, dan teknologi informasi secara umum.</p>
                                 </div>
                             </li>
@@ -164,7 +136,7 @@
 
                 <div class="homebarsec4" id="photo-id">
                     <div class="homebarsec4-title-bg">
-                        <h2>Photo's Team</h2>
+                        <h2>Dokumentasi</h2>
                     </div>
                     <div class="homebarsec4-bg">
                         <div class="homebar4">
@@ -219,6 +191,18 @@
                             <li>
                                 <div class="card-homebar5">
                                     <div class="logo-card-homebar5">
+                                        <img src="{{ asset('images/sosmed.png') }}" alt="ig">
+                                    </div>
+                                    <h2 style="font-size: 14px; text-align:center">Sosial Media Bawaslu</h2>
+                                    <h2 style="font-size: 14px; text-align:center">instagram : @bawaslukotasurabaya</h2>
+                                    <h2 style="font-size: 14px; text-align:center">youtube : @bawaslukotasurabaya1203</h2>
+                                    <h2 style="font-size: 14px; text-align:center">facebook : Bawaslu Kota Surabaya</h2>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="card-homebar5">
+                                    <div class="logo-card-homebar5">
                                         <img src="{{ asset('images/email.png') }}" alt="email">
                                     </div>
                                     <h3>f.totty.valentino.yohanes-2020@fst.unair.ac.id</h3>
@@ -228,17 +212,6 @@
                                 </div>
                             </li>
 
-                            <li>
-                                <div class="card-homebar5">
-                                    <div class="logo-card-homebar5">
-                                        <img src="{{ asset('images/phone.png') }}" alt="phone">
-                                    </div>
-                                    <h2 style="font-size: 14px">087762505591</h2>
-                                    <h2 style="font-size: 14px">085156644881</h2>
-                                    <h2 style="font-size: 14px">081353881019</h2>
-                                    <h2 style="font-size: 14px">082231521894</h2>
-                                </div>
-                            </li>
                         </div>
                     </div>
                 </div>
